1a2b7f010c41f366025f95e63426baad4eedff42
larissabrezende <larissa.abrezende@gmail.com>
2024-11-18 19:41:20 -0300

    Create navbar with links and styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,37 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../assets/Home-removebg-preview.png";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Dashboard</Link></li>
-        <li><Link to="/products">Produtos</Link></li>
-        <li><Link to="/stock-update">Atualizar Estoque</Link></li>
-        <li><Link to="/reports">Relatórios</Link></li>
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="Logo" className="logo-image" />
+        </Link>
+      </div>
+      <ul className="navbar-list">
+        <li>
+          <Link to="/" className="navbar-link">
+            Dashboard
+          </Link>
+        </li>
+        <li>
+          <Link to="/products" className="navbar-link">
+            Produtos
+          </Link>
+        </li>
+        <li>
+          <Link to="/stock-update" className="navbar-link">
+            Atualizar Estoque
+          </Link>
+        </li>
+        <li>
+          <Link to="/reports" className="navbar-link">
+            Relatórios
+          </Link>
+        </li>
       </ul>
     </nav>
   );
